7bbb8b389229387f774a8993ea4bc854222e0419
enzo-garofalo <enzo.gp3@puccampinas.edu.br>
2024-07-22 14:04:44 -0300

    Convertendo!

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for
 from flask_qrcode import QRcode
 
 app = Flask(__name__)
@@ -6,7 +6,13 @@ QRcode(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', page_title="Digite a Frase")
+
+@app.route('/converter', methods=['POST',])
+def converter():
+    frase = request.form['frase']
+    print(frase)
+    return render_template('resultado.html', page_title="Frase em QR-Code", frase=frase)
 
 
 if __name__ == '__main__':
